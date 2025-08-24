89045870908fbeb17e23dbd2d622846f49cbed21
Eduardo Molina <eduardo@azulsystems.com.br>
2025-08-24 16:15:24 -0300

    corrigindo imports

--- src/database/seed.php
+++ src/database/seed.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 // Carrega configuração
 $config = require __DIR__ . '/../config/config.php';
